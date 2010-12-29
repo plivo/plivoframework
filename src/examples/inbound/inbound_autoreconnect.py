b2720c569821f5c4b59b8c66b1232106701b9ccc
tamiel <michael.ricordeau@gmail.com>
2010-12-29 17:59:38 +0100

    inbound_autoreconnect example

---
+++ src/examples/inbound/inbound_autoreconnect.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import gevent
+from telephonie.core.inboundsocket import InboundEventSocket
+from telephonie.core.errors import ConnectError
+from telephonie.utils.logger import StdoutLogger
+
+
+class MyInboundEventSocket(InboundEventSocket):
+    '''Inbound eventsocket connector that automatically reconnects 
+    when the freeswitch eventsocket module closed the connection
+    '''
+    def __init__(self, host, port, password, filter="ALL", pool_size=500, connect_timeout=5):
+        InboundEventSocket.__init__(self, host, port, password, filter, pool_size, connect_timeout)
+        self.log = StdoutLogger()
+
+    def start(self):
+        self.log.info("Start Inbound socket %s:%d with filter %s" \
+            % (self.transport.host, self.transport.port, self._filter))
+        while True:
+            try:
+                self.connect()
+                self.log.info("Inbound socket connected")
+                self.serve_forever()
+            except ConnectError, e:
+                self.log.error("ConnectError: %s" % e.message)
+            except (SystemExit, KeyboardInterrupt): 
+                break
+            self.log.error("Inbound socket closed, try to reconnect ...")
+            gevent.sleep(1.0)
+        self.log.info("Inbound socket terminated")
+        
+
+if __name__ == '__main__':
+    c = MyInboundEventSocket('127.0.0.1', 8021, 'ClueCon')
+    c.start()    
+
