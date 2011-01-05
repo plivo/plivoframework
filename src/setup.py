acbbac822bff2b1ef9169256f4404a1a22dd537b
tamiel <michael.ricordeau@gmail.com>
2011-01-05 15:24:18 +0100

    rework setup

--- src/setup.py
+++ src/setup.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
 from telephonie import (__version__, __author__, __author_email__, __maintainer__, __maintainer_email__, __licence__)
 
-setup(name='telephonie',
-      version=__version__,
-      description='Telephonie framework',
-      url='http://github.com/miglu/Telephonie',
-      author=__author__,
-      author_email=__author_email__,
-      maintainer=__maintainer__,
-      maintainer_email=__maintainer_email__,
-      platforms=['linux'],
-      long_description='Framework to create telephony applications using FreeSWITCH',
-      packages=['telephonie', 'telephonie.core', 'telephonie.utils', 'telephonie.lib'],
-      scripts=['scripts/telephonied'],
-      keywords="telecom voip telephony freeswitch ivr",
-      license=__licence__,
-      requires=['gevent'],
-      classifiers=[
+setup_args = {
+      'name':'telephonie',
+      'version':__version__,
+      'description':'Telephonie framework',
+      'url':'http://github.com/miglu/Telephonie',
+      'author':__author__,
+      'author_email':__author_email__,
+      'maintainer':__maintainer__,
+      'maintainer_email':__maintainer_email__,
+      'platforms':['linux'],
+      'long_description':'Framework to create telephony applications using FreeSWITCH',
+      'packages':['telephonie', 'telephonie.core', 'telephonie.utils', 'telephonie.lib'],
+      'scripts':['scripts/telephonied'],
+      'keywords':"telecom voip telephony freeswitch ivr",
+      'license':__licence__,
+      'classifiers':[
         "Programming Language :: Python",
         "Operating System :: POSIX",
         "Topic :: Internet",
@@ -32,5 +31,14 @@ setup(name='telephonie',
         "Intended Audience :: Telecommunications Industry",
         "License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)",
         "Development Status :: 2 - Pre-Alpha"]
-     )
+}
 
+
+try:
+    from setuptools import setup
+    setup_args['install_requires'] = ['gevent']
+except ImportError:
+    from distutils.core import setup
+    setup_args['requires'] = ['gevent']
+  
+setup(**setup_args)
