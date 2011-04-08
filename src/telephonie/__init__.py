125f327ec8fc6d6ef27daba992c877cc5bc7aeb7
tamiel <michael.ricordeau@gmail.com>
2011-04-08 16:15:22 +0200

    up 0.3.0

--- src/telephonie/__init__.py
+++ src/telephonie/__init__.py
@@ -7,7 +7,7 @@ Primary Authors - Michael Ricordeau (tamiel) and Miglu (miglu)
 Telephonie is inspired by and uses code from - https://github.com/fiorix/eventsocket
 """
 
-__version__ = "0.0.3"
+__version__ = "0.3.0"
 
 __name__ = "telephonie"
 
