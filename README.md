54792132efd52e4d695572edc6935d5cc045936c
bevenky <venky@miglu.com>
2011-04-27 19:01:17 +0530

    Add Todo, Update License and Readme file

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Currently, it supports FreeSWITCH. Future support is planned for XMPP, Asterisk 
 
 ## License
 
-(c) 2011 Miglu Technologies Pvt Ltd
+Copyright (c) 2011 Plivo Team <contact@plivo.org>
 
 Plivo is distributed under the terms of the Mozilla Public License Version 1.1, see `LICENSE`.
 
--- setup.py
+++ setup.py
@@ -13,19 +13,19 @@ licence = "MPL 1.1"
 setup_args = {
       'name':'plivo',
       'version':'0.4.0',
-      'description':'Plivo framework',
+      'description':'Plivo - Rapid Communication Application Development Framework',
       'url':'http://github.com/miglu/Plivo',
       'author':author,
       'author_email':author_email,
       'maintainer':maintainer,
       'maintainer_email':maintainer_email,
       'platforms':['linux'],
-      'long_description':'Framework to create telephony applications',
+      'long_description':'Framework to create communication applications rapidly in any language',
       'packages':find_packages('src'),
       'package_dir':{'': 'src'},
       'include_package_data':True,
       'scripts':['src/scripts/plivod'],
-      'keywords':"telecom voip telephony freeswitch ivr",
+      'keywords':"telecom voip telephony freeswitch ivr rest",
       'license':licence,
       'zip_safe':False,
       'classifiers':[
@@ -51,6 +51,5 @@ try:
 except ImportError:
     from distutils.core import setup
     setup_args['requires'] = ['gevent']
-  
-setup(**setup_args)
 
+setup(**setup_args)
