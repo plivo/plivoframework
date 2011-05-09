f99a54f0181833f0f9b1b82b5d72a467ec46dbbf
bevenky <venky@miglu.com>
2011-05-09 09:58:40 +0530

    Merge Install script to one for CentOS and debian based systems.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
 # Plivo Installation script for CentOS 5.5/5.6
+# and Debian based distros (Debian 5.0 , Ubuntu 10.04 and above)
 # Copyright (c) 2011 Plivo Team. See LICENSE for details.
 
 
 PLIVO_CONF_PATH=https://github.com/miglu/plivo/blob/master/src/config/default.conf
-PLIVO_EZSETUP_PATH=https://github.com/miglu/plivo/blob/master/scripts/centos/ez_setup.py
 
 #####################################################
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 PLIVO_ENV=$1
 
-CURRENT=$PWD
 
 # Check if Install Directory Present
 if [ ! $1 ] || [ -z "$1" ] ; then
@@ -22,20 +21,22 @@ if [ ! $1 ] || [ -z "$1" ] ; then
 fi
 [ -d $PLIVO_ENV ] && echo "Abort. $PLIVO_ENV already exists !" && exit 1
 
-
 # Set full path
 echo "$PLIVO_ENV" |grep '^/' -q && REAL_PATH=$PLIVO_ENV || REAL_PATH=$PWD/$PLIVO_ENV
 
 
 # Identify Linix Distribution type
-if [ -f /etc/redhat-release ] ; then
+if [ -f /etc/debian_version ] ; then
+        DIST='DEBIAN'
+elif [ -f /etc/redhat-release ] ; then
         DIST='CENTOS'
 else
     echo ""
-    echo "This Installer should be run on a CentOS system"
+    echo "This Installer should be run on a CentOS or a Debian based system"
     echo ""
     exit 1
 fi
+
 
 clear
 echo ""
@@ -48,32 +49,27 @@ read INPUT
 PY_MINOR_VERSION=$(python --version 2>&1 | sed 's/Python[[:space:]]\+[0-9]\+\.\([0-9]\+\).*/\1/')
 
 
-echo "Setting up Prerequisites and Dependencies"
+if [ $(echo "$PY_MAJOR_VERSION < 4" | bc) -eq 1  ] ; then
 
-if [ $(echo "$PY_MINOR_VERSION < 5" | bc) -eq 1  ] ; then
+    echo ""
+    echo "Python version below 2.4. Please Upgrade your python."
+    echo ""
+    exit 1
 
-    yum -y install autoconf automake libtool gcc-c++ ncurses-devel make wget curl-devel curl fileutils expat-devel libxml2 libxml2-devel gettext-devel libevent
-
-    # Install Python 2.6
-    mkdir $REAL_PATH
-    mkdir $REAL_PATH/python
-    export DEPLOY=$REAL_PATH/python
-    mkdir $REAL_PATH/source
-    cd $REAL_PATH/source
-    wget http://www.python.org/ftp/python/2.6.6/Python-2.6.6.tgz
-    tar -xvf Python-2.6.6.tgz
-    cd Python-2.6.6
-    ./configure —prefix=$DEPLOY
-    make
-    make install
-
-cd $REAL_PATH/source
-wget $PLIVO_EZSETUP_PATH
-python ez_setup.py
-
-easy_install --prefix $DEPLOY virtualenv
-easy_install --prefix $DEPLOY pip
 fi
+
+echo "Setting up Prerequisites and Dependencies"
+case $DIST in
+        'DEBIAN')
+            apt-get -y install python-setuptools python-dev build-essential
+        ;;
+        'CENTOS')
+            yum -y install python-setuptools python-tools python-devel
+        ;;
+esac
+
+easy_install virtualenv
+easy_install pip
 
 # Setup virtualenv
 virtualenv --no-site-packages $REAL_PATH
@@ -88,10 +84,10 @@ wget $PLIVO_CONF_PATH
 mv default.conf $REAL_PATH/etc/plivo/
 $REAL_PATH/bin/plivo-postinstall &>/dev/null
 
-cd $CURRENT
 
 # Install Complete
 clear
+echo ""
 echo ""
 echo ""
 echo "**************************************************************"
@@ -109,5 +105,4 @@ echo "**************************************************************"
 echo ""
 echo ""
 echo "Visit http://www.plivo.org for documentation and examples"
-echo ""
 exit 0
