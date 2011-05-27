1ffc528803321c64eb4b6f7ba3ccbfa8f72b569b
tamiel <michael.ricordeau@gmail.com>
2011-05-27 15:26:08 +0200

    remove upgrade

--- freeswitch/install.sh
+++ freeswitch/install.sh
@@ -40,7 +40,6 @@ echo "Setting up Prerequisites and Dependencies for FreeSWITCH"
 case $DIST in
     'DEBIAN')
         apt-get -y update
-        apt-get -y upgrade
         apt-get -y install autoconf automake autotools-dev binutils bison build-essential cpp curl flex g++ gcc git-core libaudiofile-dev libc6-dev libdb-dev libexpat1 libgdbm-dev libgnutls-dev libmcrypt-dev libncurses5-dev libnewt-dev libpcre3 libpopt-dev libsctp-dev libsqlite3-dev libtiff4 libtiff4-dev libtool libx11-dev libxml2 libxml2-dev lksctp-tools lynx m4 make mcrypt ncftp nmap openssl sox sqlite3 ssl-cert ssl-cert unixodbc-dev unzip zip zlib1g-dev zlib1g-dev
         ;;
     'CENTOS')
