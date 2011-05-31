6c6c1a1e1a13e3a6d78ef3128c67d3a4fce0fd1a
tamiel <michael.ricordeau@gmail.com>
2011-05-31 16:16:36 +0200

    tweak JsonEvent

--- src/plivo/core/freeswitch/eventtypes.py
+++ src/plivo/core/freeswitch/eventtypes.py
@@ -218,13 +218,8 @@ class CommandResponse(Event):
         return self.is_reply_text_success()
 
 
-class JsonEvent(object):
+class JsonEvent(Event):
     '''Json Event class'''
-    __slots__ = ('__weakref__',
-                 '_headers',
-                 '_raw_body',
-                )
-
     def __init__(self, buffer=""):
         self._headers = {}
         self._raw_body = ''
@@ -235,98 +230,3 @@ class JsonEvent(object):
             except KeyError:
                 pass
 
-    def __getitem__(self, key):
-        return self.get_header(key)
-
-    def __setitem__(self, key, value):
-        self.set_header(key, value)
-
-    def get_content_length(self):
-        '''
-        Gets Content-Length header as integer.
-
-        Returns 0 If length not found.
-        '''
-        length = self.get_header('Content-Length')
-        if length:
-            try:
-                return int(length)
-            except:
-                return 0
-        return 0
-
-    def get_reply_text(self):
-        '''
-        Gets Reply-Text header as string.
-
-        Returns None if header not found.
-        '''
-        return self.get_header('Reply-Text')
-
-    def is_reply_text_success(self):
-        '''
-        Returns True if ReplyText header begins with +OK.
-
-        Returns False otherwise.
-        '''
-        reply = self.get_reply_text()
-        return reply and reply[:3] == '+OK'
-
-    def get_content_type(self):
-        '''
-        Gets Content-Type header as string.
-
-        Returns None if header not found.
-        '''
-        return self.get_header('Content-Type')
-
-    def get_headers(self):
-        '''
-        Gets all headers as a python dict.
-        '''
-        return self._headers
-
-    def set_headers(self, headers):
-        '''
-        Sets all headers from dict.
-        '''
-        self._headers = headers.copy()
-
-    def get_header(self, key, defaultvalue=None):
-        '''
-        Gets a specific header as string.
-
-        Returns None if header not found.
-        '''
-        try:
-            return self._headers[key]
-        except KeyError:
-            return defaultvalue
-
-    def set_header(self, key, value):
-        '''
-        Sets a specific header.
-        '''
-        self._headers[key.strip()] = unquote(value.strip())
-
-    def get_body(self):
-        '''
-        Gets raw Event body.
-        '''
-        return self._raw_body
-
-    def set_body(self, data):
-        '''
-        Sets raw Event body.
-        '''
-        self._raw_body = data
-
-    def is_empty(self):
-        '''Return True if no headers and no body.'''
-        return not self._raw_body and not self._headers
-
-    def __str__(self):
-        return '<%s headers=%s, body=%s>' \
-               % (self.__class__.__name__,
-                  str(self._headers),
-                  str(self._raw_body))
