343ecbc17934446c30d71c763a83cf7d16ef31f1
tamiel <michael.ricordeau@gmail.com>
2011-06-11 13:20:54 +0200

    tune servers

--- src/plivo/core/freeswitch/outboundsocket.py
+++ src/plivo/core/freeswitch/outboundsocket.py
@@ -15,6 +15,8 @@ from plivo.core.freeswitch.transport import OutboundTransport
 from plivo.core.errors import ConnectError
 
 
+BACKLOG = 2048
+
 
 class OutboundEventSocket(EventSocket):
     '''
@@ -91,11 +93,22 @@ class OutboundServer(StreamServer):
     '''
     FreeSWITCH Outbound Event Server
     '''
+    # Sets the maximum number of consecutive accepts that a process may perform on
+    # a single wake up. High values give higher priority to high connection rates,
+    # while lower values give higher priority to already established connections.
+    max_accept = 5000
+
+    # the number of seconds to sleep in case there was an error in accept() call
+    # for consecutive errors the delay will double until it reaches max_delay
+    # when accept() finally succeeds the delay will be reset to min_delay again
+    min_delay = 0.001
+    max_delay = 0.1
+
     def __init__(self, address, handle_class, filter="ALL"):
         self._filter = filter
         #Define the Class that will handle process when receiving message
         self._handle_class = handle_class
-        StreamServer.__init__(self, address, self.do_handle)
+        StreamServer.__init__(self, address, self.do_handle, backlog=BACKLOG)
 
     def do_handle(self, socket, address):
         self._handle_class(socket, address, self._filter)
