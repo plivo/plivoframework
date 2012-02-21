a1072a4d289671741374c83b1712b5326474105b
Michael Ricordeau <mike@plivo.com>
2012-02-21 19:03:38 +0100

    fix check uuid exists

--- src/plivo/rest/freeswitch/outboundsocket.py
+++ src/plivo/rest/freeswitch/outboundsocket.py
@@ -484,7 +484,8 @@ class PlivoOutboundEventSocket(OutboundEventSocket):
                 # double check channel exists/hung up
                 if self.has_hangup():
                     raise RESTHangup()
-                if self.api('uuid_exists %s' % self.get_channel_unique_id()) != 'true':
+                res = self.api('uuid_exists %s' % self.get_channel_unique_id()):
+                if res.get_response() != 'true':
                     self.log.warn("Call doesn't exist !")
                     raise RESTHangup()
                 # Set target URL to Redirect URL
