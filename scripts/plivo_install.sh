56d814828f22d9ca52ad09c89ab91d16ba7b647d
Michael Ricordeau <mike@plivo.com>
2013-12-09 15:10:36 -0800

    fix gevent install

--- scripts/plivo_install.sh
+++ scripts/plivo_install.sh
@@ -184,8 +184,8 @@ virtualenv --no-site-packages $REAL_PATH
 source $REAL_PATH/bin/activate
 
 # force installation of gevent 1.03a
-pip install --upgrade -f http://gevent.googlecode.com/files/gevent-1.0a3.tar.gz gevent
-
+pip uninstall gevent
+pip install -Iv http://gevent.googlecode.com/files/gevent-1.0a3.tar.gz
 pip install -e git+${PLIVO_GIT_REPO}@${BRANCH}#egg=plivo
 
 
