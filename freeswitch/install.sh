280564c60f7c93b5199239428f51af0f587afed1
D. Yu <dyu+honeypot@fastmail.fm>
2014-10-01 10:05:07 +0000

    freeswitch: use the stable branch
    Easier if it's pegged at a known stable version instead of
    using master all the time and unknowingly using different versions
    on different machines.

--- freeswitch/install.sh
+++ freeswitch/install.sh
@@ -78,6 +78,7 @@ esac
 cd $FS_BASE_PATH
 git clone $FS_GIT_REPO --depth=1
 cd $FS_BASE_PATH/freeswitch
+git checkout -b v1.2.stable origin/v1.2.stable
 sh bootstrap.sh && ./configure --prefix=$FS_INSTALLED_PATH || exit 1
 [ -f modules.conf ] && cp modules.conf modules.conf.bak
 sed -i \
